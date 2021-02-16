036eab658851d6c2c1eb9b8f07b437ca22e66aaa
sjimen13 <sjimen13@gmail.com>
2021-02-16 11:11:03 -0500

    day1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+console.log('scrpt');
+const keys = document.querySelectorAll('.key');
+
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (!audio) return; //stop the function from running
+  audio.currentTime = 0; //rewind to the start
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound(e));
